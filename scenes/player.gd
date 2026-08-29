89a4e46e12860bf86bd0e79edd3cb0622c85da11
vii-abdullatif <dows5750@gmail.com>
2026-08-29 13:48:36 +0300

    Initial commit

---
+++ scenes/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
